84d325e96092cc292ccff088cf955adbda3af64e
matt manes <mattmanes@pm.me>
2023-10-12 13:48:50 -0500

    Initial commit

--- scrapers/moveworks.py
+++ scrapers/moveworks.py
@@ -2,8 +2,8 @@ from pathier import Pathier
 
 root = Pathier(__file__).parent
 (root.parent).add_to_PATH()
-from jobgruel import AshbyGruel
+from jobgruel import GreenhouseGruel
 
 
-class JobScraper(AshbyGruel):
+class JobScraper(GreenhouseGruel):
     ...
