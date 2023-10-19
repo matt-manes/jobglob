c58a8604d7ee6a7f841a596abd3a46a6c2a274b9
matt manes <mattmanes@pm.me>
2023-10-18 23:09:58 -0500

    perf: better output

--- check_listings.py
+++ check_listings.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import requests
+from griddle import griddy
 from printbuddies import PoolBar
 
 from jobbased import JobBased
@@ -25,13 +26,14 @@ def get_pool(listings: list[dict]) -> PoolBar:
     )
 
 
-def check_table(table: str):
+def check_table(table: str) -> list[dict]:
     print(f"Checking {table}...")
     with JobBased() as db:
         listings = db.select(table, where="alive = 1", order_by="RANDOM()")
     pool = get_pool(listings)
     results = pool.execute()
     dead_count = 0
+    dead_listings = []
     failed_requests = []
     for result in results:
         if result["alive"] is None:
@@ -47,6 +49,7 @@ def check_table(table: str):
                     datetime.now(),
                     f"listing_id = {listing['listing_id']}",
                 )
+            dead_listings.append(listing)
     if not dead_count:
         print("Did not find any dead listings.")
     else:
@@ -55,12 +58,34 @@ def check_table(table: str):
         print("Failed requests:")
         for request in failed_requests:
             print(f"  {request}")
+    return dead_listings
 
 
 if __name__ == "__main__":
     for table in ["scraped_listings", "listings"]:
         try:
-            check_table(table)
+            dead_listings = check_table(table)
+            if table == "listings":
+                ids = (
+                    "("
+                    + ", ".join(str(listing["listing_id"]) for listing in dead_listings)
+                    + ")"
+                )
+                with JobBased() as db:
+                    listings = db.select(
+                        "listings",
+                        [
+                            "listing_id AS id",
+                            "position",
+                            "companies.name AS company",
+                            "listings.url AS url",
+                        ],
+                        [
+                            "INNER JOIN companies ON listings.company_id = companies.company_id"
+                        ],
+                        where=f"listings.listing_id IN {ids}",
+                    )
+                print(db.to_grid(listings))
         except Exception as e:
             print(e)
     input("...")
