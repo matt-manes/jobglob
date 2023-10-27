faaf2d67658ded3e4bf06c87f16e85d1c8b42b90
matt manes <mattmanes@pm.me>
2023-10-27 00:38:37 -0500

    Initial commit

---
+++ scrapers/kaia_health.py
@@ -0,0 +1,19 @@
+from pathier import Pathier
+
+root = Pathier(__file__).parent
+(root.parent).add_to_PATH()
+import jobgruel
+
+
+class JobScraper(jobgruel.GreenhouseGruel):
+    ...
+
+
+if __name__ == "__main__":
+    import helpers
+    from datetime import datetime, timedelta
+
+    start = datetime.now() - timedelta(seconds=2)
+    j = JobScraper()
+    j.scrape()
+    print(helpers.load_log(j.board.company.name).filter_dates(start))
