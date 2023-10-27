7884dcff2de3b5f2d5a6d12bf904e9fda84fef84
matt manes <mattmanes@pm.me>
2023-10-27 00:31:19 -0500

    perf: print number of boards added

--- bulk_add_scrapable_boards.py
+++ bulk_add_scrapable_boards.py
@@ -11,6 +11,7 @@ def main():
     with JobBased() as db:
         existing_board_urls = [board.url for board in db.boards]
     boards = Pathier(__file__).with_suffix(".txt").split()
+    num_boards_added = 0
     for board in boards:
         url, company = board.split(maxsplit=1)
         url = url.strip("/")
@@ -18,6 +19,8 @@ def main():
             with JobBased() as db:
                 db.add_board(url, company)
                 helpers.create_scraper_from_template(url, company)
+            num_boards_added += 1
+    print(f"Added {num_boards_added} boards.")
 
 
 if __name__ == "__main__":
