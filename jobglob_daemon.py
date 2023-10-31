6ccb48534f054a463ccd4cb6a556455da0f23548
matt manes <mattmanes@pm.me>
2023-10-31 15:24:39 -0500

    perf: improve sleep time printout and prevent running on weekends

--- jobglob_daemon.py
+++ jobglob_daemon.py
@@ -1,51 +1,118 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
+import loggi
+from noiftimer import Timer
 from pathier import Pathier
+from printbuddies import print_in_place
 
 from jobglob import JobGlob
-from noiftimer import Timer
 
 root = Pathier(__file__).parent
 
 
-def is_business_hours() -> bool:
-    now = datetime.now()
-    if now.replace(hour=8) <= now <= now.replace(hour=17):
-        return True
-    return False
+class JobGlobDaemon:
+    def __init__(self):
+        self._last_glob_time = None
 
+    @property
+    def business_hours(self) -> tuple[int, int]:
+        # Assuming local time is CST: start 8 am EST and end 5 pm PST
+        # (7 am - 7 pm CST)
+        return (7, 19)
 
-def get_last_brew_time() -> datetime:
-    logpath = root / "jobglob.log"
-    if logpath.exists():
-        logs = (root / "jobglob.log").split()[::-1]
-        for log in logs:
-            if "Brew complete." in log:
-                date = log.split("|-|")[1]
-                return datetime.strptime(date, "%m/%d/%Y %I:%M:%S %p")
-    return datetime.fromtimestamp(0)
+    @property
+    def is_weekend(self) -> bool:
+        return datetime.now().weekday in [5, 6]
 
+    @property
+    def glob_interval(self) -> int:
+        """Number of seconds between globbings."""
+        return 3600
 
-def check_last_brew_time():
-    last_brew_time = get_last_brew_time()
-    seconds_since_last_brew = (datetime.now() - last_brew_time).total_seconds()
-    if seconds_since_last_brew < 3600:
-        sleep_time = 3600 - seconds_since_last_brew
-        print(f"Sleeping for {Timer.format_time(sleep_time)}")
-        time.sleep(sleep_time)
+    @property
+    def logpath(self) -> Pathier:
+        return root / "jobglob.log"
 
+    @property
+    def seconds_since_last_glob(self) -> float:
+        return (datetime.now() - self.last_glob_time).total_seconds()
 
-def main():
-    while True:
-        if is_business_hours():
-            check_last_brew_time()
+    @property
+    def last_glob_time(self) -> datetime:
+        if not self._last_glob_time:
+            if self.logpath.exists():
+                logs = loggi.load_log(self.logpath).filter_messages(["Brew complete"])
+                if logs.num_events:
+                    self._last_glob_time = logs.events[-1].date
+            else:
+                self._last_glob_time = datetime.fromtimestamp(0)
+        assert self._last_glob_time
+        return self._last_glob_time
+
+    @last_glob_time.setter
+    def last_glob_time(self, time: datetime):
+        self._last_glob_time = time
+
+    @property
+    def seconds_until_next_glob(self) -> float:
+        if self.is_weekend:
+            return self.seconds_until_monday_business_start
+        if not self.is_business_hours:
+            return self.seconds_until_business_hours
+        return self.glob_interval - self.seconds_since_last_glob
+
+    @property
+    def is_business_hours(self) -> bool:
+        now = datetime.now()
+        if (
+            now.replace(hour=self.business_hours[0], minute=0, second=0, microsecond=0)
+            <= now
+            <= now.replace(
+                hour=self.business_hours[1], minute=0, second=0, microsecond=0
+            )
+        ):
+            return True
+        return False
+
+    @property
+    def seconds_until_monday_business_start(self) -> float:
+        now = datetime.now()
+        monday = (now + timedelta(days=7 - now.weekday())).replace(
+            hour=self.business_hours[0], minute=0, second=0, microsecond=0
+        )
+        return (monday - now).total_seconds()
+
+    @property
+    def seconds_until_business_hours(self) -> float:
+        now = datetime.now()
+        start, stop = self.business_hours
+        business_start = now.replace(hour=start, minute=0, second=0, microsecond=0)
+        business_stop = now.replace(hour=stop, minute=0, second=0, microsecond=0)
+        if now < business_start:
+            return (business_start - now).total_seconds()
+        if business_stop < now:
+            return ((business_start + timedelta(days=1)) - now).total_seconds()
+        return 0
+
+    def nap(self):
+        while (seconds := self.seconds_until_next_glob) > 0:
+            print_in_place(f"Sleeping for {Timer.format_time(seconds)}", True)
+            time.sleep(60)
+
+    def run(self):
+        while True:
+            self.nap()
             jobglob = JobGlob(["JobScraper"], ["*template.py"], root / "scrapers")
             print(f"Brewing at {datetime.now():%m/%d %I:%M %p}")
             jobglob.brew()
-            print("nap time")
+            self.last_glob_time = datetime.now()
             del jobglob
-        time.sleep(3600)
+
+
+def main():
+    daemon = JobGlobDaemon()
+    daemon.run()
 
 
 if __name__ == "__main__":
