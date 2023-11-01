d9e514d18df66216e8e0acc1daf5fb1de9b9ed72
Matt Manes <mattmanes@pm.me>
2023-10-31 19:39:08 -0500

    fix: delete database when overwriting

--- database_init.py
+++ database_init.py
@@ -19,6 +19,8 @@ def init(db_name: str, silent_overwrite: bool = False):
             sys.exit()
         else:
             print(f"Deleting {db_path}")
+            db_path.delete()
+    print("Building database.")
     with Databased(db_path) as db:
         views = (root / "sql").glob("*_view.sql")
         db.execute_script(root / "sql" / "schema.sql")
@@ -26,6 +28,7 @@ def init(db_name: str, silent_overwrite: bool = False):
             db.execute_script(view)
         data_path = root / "sql" / "jobs_data.sql"
         if data_path.exists():
+            print("Inserting data.")
             db.execute_script(data_path)
 
 
@@ -33,7 +36,7 @@ def main():
     """ """
     print(f"Creating database...")
     init("jobs.db")
-    input("Done")
+    input("Done, press enter to close.")
 
 
 if __name__ == "__main__":
