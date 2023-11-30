a143b215e5fb918a711a68df6aede7f8c788af94
matt manes <mattmanes@pm.me>
2023-11-30 11:57:59 -0600

    chore: update data

--- scrapers/akuo_energy.py
+++ scrapers/akuo_energy.py
@@ -9,13 +9,3 @@ class JobScraper(jobgruel.SmartrecruiterGruel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.api_endpoint = "https://careers.smartrecruiters.com/Akuo/en---careers-akuo/api/groups?page="
-
-
-if __name__ == "__main__":
-    import helpers
-    from datetime import datetime, timedelta
-
-    start = datetime.now() - timedelta(seconds=2)
-    j = JobScraper()
-    j.scrape()
-    print(helpers.load_log(j.board.company.name).filter_dates(start))
