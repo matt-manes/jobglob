896bdf148e3b387546fb047ff6d6093ba1691bc7
matt manes <mattmanes@pm.me>
2023-12-01 16:04:49 -0600

    fix: fix url for listings

--- scrapers/ninedot_energy.py
+++ scrapers/ninedot_energy.py
@@ -1,4 +1,7 @@
+from bs4 import Tag
 from pathier import Pathier
+
+import models
 
 root = Pathier(__file__).parent
 (root.parent).add_to_PATH()
@@ -6,4 +9,8 @@ import jobgruel
 
 
 class JobScraper(jobgruel.GreenhouseGruel):
-    ...
+    def parse_item(self, item: Tag) -> models.Listing | None:
+        listing = super().parse_item(item)
+        if listing:
+            listing.url = listing.url.replace("work-with-us", "work-with-us/apply")
+        return listing
