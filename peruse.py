d00276453192f649cca11285ae2ca88c33583e5c
matt manes <mattmanes@pm.me>
2024-01-08 19:51:30 -0600

    perf: Generate peruse_filters.toml if it doesn't exist and add separate cli args for filtering by position and location.

--- peruse.py
+++ peruse.py
@@ -5,6 +5,7 @@ from dataclasses import asdict
 from griddle import griddy
 from pathier import Pathier
 
+import helpers
 import models
 from jobbased import JobBased
 
@@ -12,9 +13,21 @@ root = Pathier(__file__).parent
 
 import argparse
 
+""" 
+Go through unseen job listings and mark them as seen after choosing to ignore them or add them to your pinned listings.
+
+Using this script means you don't have to keep looking at listings you've already seen.
+"""
+
 
 def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description=""" Look through newly added job listings.
+        If there is no `peruse_filters.toml` file, it will be created.
+        The fields in this file can be used to filter locations and positions by text as well as set up default search terms.
+        All fields are case insensitive.
+        """
+    )
 
     parser.add_argument(
         "key_terms",
@@ -25,10 +38,18 @@ def get_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "-f",
+        "-fl",
+        "--filter_locations",
+        action="store_true",
+        help=""" Use `filter_out_location_terms` in `peruse_filters.toml` to filter listings. 
+        i.e. any listings with these words in the job `location` won't be shown.""",
+    )
+
+    parser.add_argument(
+        "-fp",
         "--filter_positions",
         action="store_true",
-        help=""" Use `peruse_filters.toml` to filter listings. 
+        help=""" Use `filter_out_position_terms` in `peruse_filters.toml` to filter listings. 
         i.e. any listings with these words in the job `position` won't be shown.
         Overrides `key_terms` arg.""",
     )
@@ -37,19 +58,31 @@ def get_args() -> argparse.Namespace:
         "-ds",
         "--default_search",
         action="store_true",
-        help=""" Use the default search terms in `persuse_filters.toml` in addition to any provided `key_terms` arguments. """,
+        help=""" Use `default_search_terms` in `persuse_filters.toml` in addition to any provided `key_terms` arguments. """,
     )
 
     parser.add_argument(
         "-nf",
         "--newest_first",
         action="store_true",
-        help=""" Go through listings starting with the most recent. """,
+        help=""" Go through listings starting with the most recent.
+        Default is oldest first.""",
     )
 
     args = parser.parse_args()
+    args.key_terms = [term.lower() for term in args.key_terms]
 
     return args
+
+
+def load_filters() -> dict[str, list]:
+    """Load filters from `peruse_filters.toml`.
+    Create an empty file from template if it doesn't exist."""
+    filter_path = root / "peruse_filters.toml"
+    if not filter_path.exists():
+        helpers.create_peruse_filters_from_template()
+    filters = (root / "peruse_filters.toml").loads()
+    return {key: [text.lower() for text in filters[key]] for key in filters}
 
 
 def filter_listings(
@@ -58,8 +91,8 @@ def filter_listings(
     key_terms: list[str],
     exclude_terms: list[str],
 ) -> list[models.Listing]:
+    """Filter `listings` on the field specified by `filter_on`."""
     filtered_listings = []
-
     for listing in listings:
         column = getattr(listing, filter_on).lower()
         if any(exclude in column for exclude in exclude_terms):
@@ -71,6 +104,7 @@ def filter_listings(
 
 
 def do_action(listing: models.Listing):
+    """Take input and perform the desired action for `listing`."""
     while True:
         action = input(
             "Enter action ('a': add listing, 'd': mark dead, 'o': open url, 'q': quit, 'i' to ignore and mark seen): "
@@ -98,6 +132,7 @@ def do_action(listing: models.Listing):
 
 
 def show(listing: models.Listing):
+    """Format and print a listing in the terminal."""
     line = asdict(listing)
     line = {
         "id": line["id_"],
@@ -110,26 +145,36 @@ def show(listing: models.Listing):
     print(griddy([line], "keys"))
 
 
-def main(args: argparse.Namespace):
-    with JobBased() as db:
-        listings = db.unseen_live_listings
-    if args.newest_first:
-        listings = listings[::-1]
-    filters = (root / "peruse_filters.toml").loads()
-    default_search = filters["search"] if args.default_search else []
-    listings = filter_listings(
-        listings, "location", [], exclude_terms=filters["location"]
-    )
-    excludes = filters["position"] if args.filter_positions else []
-    listings = filter_listings(
-        listings, "position", args.key_terms + default_search, excludes
-    )
+def peruse(listings: list[models.Listing]):
+    """Show each listing in `listings` and perform desired action."""
     num_listings = len(listings)
     print(f"Unseen listings: {num_listings}")
     for i, listing in enumerate(listings, 1):
         print(f"{i}/{num_listings}")
         show(listing)
         do_action(listing)
+
+
+def main(args: argparse.Namespace):
+    with JobBased() as db:
+        listings = db.unseen_live_listings
+    if args.newest_first:
+        listings = listings[::-1]
+    # ========================
+    # filter unseen listings
+    # ========================
+    filters = load_filters()
+    default_search = filters["default_search_terms"] if args.default_search else []
+    if args.filter_locations:
+        listings = filter_listings(
+            listings, "location", [], exclude_terms=filters["filter_out_location_terms"]
+        )
+    excludes = filters["filter_out_position_terms"] if args.filter_positions else []
+    listings = filter_listings(
+        listings, "position", args.key_terms + default_search, excludes
+    )
+    # ========================
+    peruse(listings)
 
 
 if __name__ == "__main__":
