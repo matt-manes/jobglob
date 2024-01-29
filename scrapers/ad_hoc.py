cb3255e87e997fa6c8c31a93e83f8d2aa5a04a7b
matt manes <mattmanes@pm.me>
2024-01-29 12:57:31 -0600

    perf: use api endpoint instead of selenium

--- scrapers/ad_hoc.py
+++ scrapers/ad_hoc.py
@@ -1,9 +1,4 @@
-import time
-
-from bs4 import Tag
-from gruel import ParsableItem
 from pathier import Pathier
-from seleniumuser import User
 
 root = Pathier(__file__).parent
 (root.parent).add_to_PATH()
@@ -12,26 +7,78 @@ from jobgruel import JobGruel
 
 
 class JobScraper(JobGruel):
-    def get_parsable_items(self) -> list[ParsableItem]:
-        with User(True) as user:
-            user.get(self.board.url)
-            time.sleep(1)
-            soup = user.get_soup()
-        return soup.find_all("div", class_="opportunity")
+    @property
+    def api_url(self) -> str:
+        return f"{self.board.url}/JobBoardView/LoadSearchResults"
 
-    def parse_item(self, item: Tag) -> models.Listing | None:
-        url = "https://adhoc.rec.pro.ukg.net"
+    @property
+    def api_payload(self) -> dict:
+        return {
+            "opportunitySearch": {
+                "Top": 1000,
+                "Skip": 0,
+                "QueryString": "",
+                "OrderBy": [
+                    {
+                        "Value": "postedDateDesc",
+                        "PropertyName": "PostedDate",
+                        "Ascending": False,
+                    }
+                ],
+                "Filters": [
+                    {
+                        "t": "TermsSearchFilterDto",
+                        "fieldName": 4,
+                        "extra": None,
+                        "values": [],
+                    },
+                    {
+                        "t": "TermsSearchFilterDto",
+                        "fieldName": 5,
+                        "extra": None,
+                        "values": [],
+                    },
+                    {
+                        "t": "TermsSearchFilterDto",
+                        "fieldName": 6,
+                        "extra": None,
+                        "values": [],
+                    },
+                ],
+            },
+            "matchCriteria": {
+                "PreferredJobs": [],
+                "Educations": [],
+                "LicenseAndCertifications": [],
+                "Skills": [],
+                "hasNoLicenses": False,
+                "SkippedSkills": [],
+            },
+        }
+
+    def get_parsable_items(self) -> list[dict]:
+        response = self.request(
+            self.api_url,
+            "post",
+            headers={
+                "Content-Type": "application/json",
+                "Accept-Encoding": "gzip, deflate",
+            },
+            json_=self.api_payload,
+        )
+        return response.json()["opportunities"]
+
+    def parse_item(self, item: dict) -> models.Listing | None:
         try:
             listing = self.new_listing()
-            a = item.find("a", attrs={"data-automation": "job-title"})
-            assert isinstance(a, Tag)
-            listing.url = f"{url}{a.get('href')}"
-            listing.position = a.text
-            span = item.find(
-                "span", attrs={"data-automation": "city-state-zip-country-label"}
+            listing.position = item["Title"]
+            if item["Locations"]:
+                listing.location = item["Locations"][0]["LocalizedDescription"]
+            else:
+                listing.location = "Unlisted"
+            listing.url = (
+                f"{self.board.url}/OpportunityDetail?opportunityId={item['Id']}"
             )
-            assert isinstance(span, Tag)
-            listing.location = span.text
             return listing
         except Exception as e:
             self.logger.exception("Failure to parse item:")
